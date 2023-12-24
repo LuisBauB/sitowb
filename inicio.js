14e24538f7bc980d9534e4acddbe34a5872cfbe9
LuisBauB <154741893+LuisBauB@users.noreply.github.com>
2023-12-24 09:55:54 -0400

    Add files via upload

---
+++ inicio.js
@@ -0,0 +1,10 @@
+let numero1 = 40;
+let numero2 = 30;
+
+if(numero1 > numero2) {
+    console.log("El numero " + numero1 + " Es más grande.");
+} else if (numero1 < numero2) {
+console.log("El numero" + numero2 + " Es más grande");
+} else {
+    console.log("Ambos son iguales.");
+}
